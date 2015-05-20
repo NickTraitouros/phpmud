1726ae1b3932a4a5462fd945931eda3319f8115a
NickTraitouros <nick.traitouros@gmail.com>
2015-05-19 21:12:42 -0400

    migrations for rooms and maps

---
+++ database/migrations/2015_05_20_005743_create_rooms_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('rooms', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('map_id')->unsigned();;
+			$table->integer('x');
+			$table->integer('y');
+			$table->timestamps();
+		});
+
+		Schema::table('rooms', function($table) {
+	        $table->foreign('map_id')->references('id')->on('maps');
+		});
+
+	}
+
+
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('rooms');
+	}
+
+}
