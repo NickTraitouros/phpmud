3f3f316714cc123ac6331376d86cb78db88f1b44
NickTraitouros <nick.traitouros@gmail.com>
2015-05-19 22:00:12 -0400

    added seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		// $this->call('UserTableSeeder');
+		$this->call('MapsTableSeeder');
+		$this->call('RoomsTableSeeder');
+		$this->call('HerosTableSeeder');
 	}
 
 }
